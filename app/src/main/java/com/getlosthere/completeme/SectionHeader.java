f73f64dda4ed909a246ee989f55d70dad63652bf
violetaria <terri.chu@outlook.com>
2016-06-16 21:18:00 -0400

    got section header working for listview

---
+++ app/src/main/java/com/getlosthere/completeme/SectionHeader.java
@@ -0,0 +1,13 @@
+package com.getlosthere.completeme;
+
+/**
+ * Created by violetaria on 6/16/16.
+ */
+public class SectionHeader{
+    public String header_text;
+
+    public SectionHeader(String header_text) {
+        super();
+        this.header_text = header_text;
+    }
+}
